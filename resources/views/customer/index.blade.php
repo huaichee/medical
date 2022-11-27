ced061929548b98164c156eee0e332d2a188f975
huaichee <huaichee89@gmail.com>
2022-11-27 09:43:24 +0800

    feat: fontawesome cdn

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -20,7 +20,7 @@
                                 <label for="search" class="sr-only">Search</label>
                                 <div class="relative">
                                     <a class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-1 rounded text-right float-right" title="reset search" href="{{ route('customer') }}">
-                                        r
+                                        <i class="fa-solid fa-rotate-left"></i>
                                     </a>
                                     <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                                         <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
